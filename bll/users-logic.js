4d693c57caf83e5973df7ccb21f84ca8d607e2ba
Yaniv <bendayaniv@gmail.com>
2024-06-10 12:44:54 +0300

    Messurment Function

--- bll/users-logic.js
+++ bll/users-logic.js
@@ -22,10 +22,34 @@ async function deleteUser(id) {
   await firebase.database.ref('users/' + id).remove();
 }
 
+async function haversineDistance(coords1, coords2) {
+  const R = 6371; // Radius of Earth in kilometers
+  const lat1 = coords1.lat;
+  const lon1 = coords1.lon;
+  const lat2 = coords2.lat;
+  const lon2 = coords2.lon;
+
+  const dLat = ((lat2 - lat1) * Math.PI) / 180;
+  const dLon = ((lon2 - lon1) * Math.PI) / 180;
+
+  const sinDLat = Math.sin(dLat / 2);
+  const sinDLon = Math.sin(dLon / 2);
+  const cosLat1 = Math.cos((lat1 * Math.PI) / 180);
+  const cosLat2 = Math.cos((lat2 * Math.PI) / 180);
+
+  const a = sinDLat * sinDLat + cosLat1 * cosLat2 * sinDLon * sinDLon;
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c;
+
+  return distance;
+}
+
 module.exports = {
   getAllUsers,
   getUserById,
   addUser,
   updateUser,
   deleteUser,
+  haversineDistance,
 };
